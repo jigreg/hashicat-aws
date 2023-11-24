8e668cd3b54dbe67bab0792202cc9858a4a40bcd
jigreg <jigreg@naver.com>
2023-11-24 08:22:32 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,14 @@
+module "s3_bucket" {
+  source  = "app.terraform.io/chip-seojun/s3-bucket/aws"
+  version = "2.8.0"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
